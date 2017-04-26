eac8786dcbe17f386d567d43fde3cd7822fab42f
MaianaB <maiana.brito@ccc.ufcg.edu.br>
2017-04-26 09:26:26 -0300

    Exercício - Maiana

---
+++ maiana/main.py
@@ -0,0 +1,110 @@
+from google.appengine.ext import ndb
+import webapp2
+import json
+from models import *
+from util import *
+
+class InstituicaoWebapp(webapp2.RequestHandler):
+	
+	def get(self):
+		
+		instituicao = self.request.get('nome')
+		
+		if instituicao:		
+			query = Instituicao.query().filter(Instituicao.nome_instituicao == instituicao)
+		
+		else:			
+			query = Instituicao.query()				
+
+		dados = [inst.to_dict() for inst in query]
+		self.response.headers['Content-Type'] = 'application/json; charset=utf-8'
+		self.response.write(data2json(dados))
+		
+	
+	def post(self):		
+		
+		dados = json.loads(self.request.body)
+		
+		cnpj = dados['cnpj']
+		
+		instituicao = Instituicao(id=cnpj)
+		instituicao.nome_instituicao = dados['nome_instituicao']
+		instituicao.nome_respons = dados['nome_respons']
+		instituicao.cnpj = cnpj
+		instituicao.natureza_juridica = dados['natureza_juridica']
+		instituicao.endereco = dados['endereco']
+		instituicao.area_atuacao = dados['area_atuacao']
+		instituicao.descricao = dados['descricao']
+		instituicao.imagem = dados.get('imagem')
+		instituicao.email = dados['email']
+		instituicao.telefone = dados['telefone']
+		
+		instituicao.put()
+		
+		self.response.write('{"id": "%s"}' % cnpj)
+		self.response.set_status(201)
+        
+
+class UsuarioWebapp(webapp2.RequestHandler):
+	
+	def get(self):
+		query = Usuario.query()
+
+		dados = [usuario.to_dict() for usuario in query]
+		self.response.headers['Content-Type'] = 'application/json; charset=utf-8'
+		self.response.write(data2json(dados))
+		
+	
+	def post(self):		
+		
+		dados = json.loads(self.request.body)
+		
+		cpf = dados['cpf']
+		
+		usuario = Usuario(id=cpf)
+		usuario.nome= dados['nome']
+		usuario.senha = dados['senha']
+		usuario.cpf = cpf
+		usuario.email = dados['email']
+		usuario.foto = dados.get('foto')
+		
+		usuario.put()
+		
+		self.response.write('{"id": "%s"}' % cpf)
+		self.response.set_status(201)
+        
+
+class PostsWebapp(webapp2.RequestHandler):
+	
+	def get(self):
+		
+		query = Posts.query()
+		
+		dados = [post.to_dict() for post in query]
+		
+		self.response.headers['Content-Type'] = 'application/json; charset=utf-8'
+		self.response.write(data2json(dados))
+		
+	
+	def post(self):		
+		
+		dados = json.loads(self.request.body)
+				
+		post = Posts()
+		post.nome_instituicao = dados['nome_instituicao']
+		post.nome_autor = dados['nome_autor']
+		post.curtidas = dados['curtidas']
+		
+		post.put()
+		
+		self.response.write('{"id": "%s"}' % post.key.integer_id())
+		self.response.set_status(201)
+        
+
+app = webapp2.WSGIApplication([
+    ('/api/instituicao', InstituicaoWebapp),
+    ('/api/usuario', UsuarioWebapp),
+    ('/api/posts', PostsWebapp),
+    
+], debug=True)
+
