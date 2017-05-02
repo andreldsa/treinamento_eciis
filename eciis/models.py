ca91ceb14fd516d5e618ec83ef3da0e067287bb8
Dalton Serey <daltonserey@gmail.com>
2017-05-02 11:39:00 -0300

    add details to models.py

--- eciis/models.py
+++ eciis/models.py
@@ -4,58 +4,86 @@ class User(ndb.Model):
 
     # The id of the institutions to which the user belongs
     # minimum = 1
-    institution = ndb.KeyStringProperty(repeated=True)
+    institutions = ndb.KeyProperty(kind="Institution", repeated=True)
 
     # The id of the institutions followed by the user
     # minimum = 0
-    follows = ndb.KeyStringProperty(repeated=True)
-
-    # The id of the posts authored by the user
-    posts = ndb.KeyStringProperty(repeated=True)
+    follows = ndb.KeyProperty(kind="Institution", repeated=True)
 
     # The ids of the institutions administered by the user
-    institutions_admin = ndb.KeyStringProperty(repeated=True)
+    institutions_admin = ndb.KeyProperty(kind="Institution", repeated=True)
 
-    # Notofications received by the user
-    notifications = ndb.JsonProperty()
+    # Notifications received by the user
+    notifications = ndb.JsonProperty(repeated=True)
 
+    # The id of the posts authored by the user
+    posts = ndb.KeyProperty(kind="Post", repeated=True)
+
+    # The id of the user timeline
+    timeline = ndb.KeyProperty(kind="Timeline")
+
+    state = ndb.StringProperty(choices=set([
+        'pending',
+        'active',
+        'inactive'
+    ]), default='pending')
+   
 
 class Timeline(ndb.Model):
 
-    owner = ndb.KeyStringProperty
-    posts = ndb.JsonProperty
-
+    # TODO: In the future think about maximum size of the entity
+    # The data of the posts
+    # The only required data is the Post Key/id
+    # Ordered by the datetime (most recent first)
+    posts = ndb.JsonProperty(repeated=True, compressed=True)
 
 
 class Institution(ndb.Model):
 
     # The admin user of this institution
-    admin = ndb.KeyStringProperty(required=True)
+    admin = ndb.KeyProperty(kind="User", required=True)
 
     # The parent institution
     # Value is None for institutions without parent
-    parent_institution = ndb.KeyStringProperty()
+    # User query to retrieve children institutions
+    parent_institution = ndb.KeyProperty(kind="Institution")
 
     # The ids of users who are members of this institution
-    members = ndb.KeyStringProperty(repeated=True)
+    members = ndb.KeyProperty(kind="User", repeated=True)
 
     # Users subscribed to this institution's posts
     # All these followers receive copies of the posts
     # of this institution in their timeline.
-    followers = ndb.KeyStringProperty(repeated=True)
+    followers = ndb.KeyProperty(kind="User", repeated=True)
 
-    # 
-    posts = ndb.KeyStringProperty(repeated=True)
+    # Posts created by members of this institution
+    posts = ndb.KeyProperty(kind="Post", repeated=True)
 
-    
-class Post(ndb.Model)
+    state = ndb.StringProperty(choices=set([
+        'pending',
+        'active',
+        'inactive'
+    ]), default='pending')
+   
+
+class Post(ndb.Model):
 
     # user who is the author
-    author = ndb.KeyStringProperty
+    author = ndb.KeyProperty(kind="User", required=True)
 
     # institution to which this post belongs
-    institution = ndb.KeyStringProperty
+    institution = ndb.KeyProperty(kind="Institution", required=True)
+
+    state = ndb.StringProperty(choices=set([
+        'draft',
+        'published',
+        'deleted'
+    ]), default='draft')
+
+    # Comments of the post
+    # Concurrency controlled by Transactions
+    comments = ndb.JsonProperty(repeated=True)
 
 
-
-class Channel(ndb.Model)
+class Channel(ndb.Model):
+    pass
