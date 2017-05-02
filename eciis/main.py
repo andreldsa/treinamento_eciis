910e7b8bd72316be090d4f541ea53b21d6a8d199
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2017-05-02 16:09:24 -0300

    add handlers of path /api/institution/post and /api/institution/notifications

--- eciis/main.py
+++ eciis/main.py
@@ -21,6 +21,35 @@ class ErroHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write("Rota Inesistente")
 
+class InstitutionPostHandler(webapp2.RequestHandler):
+
+	def get(self, institution_id, post_id):
+
+                post = Post.get_by_id(int(post_id))
+                self.response.write(post)
+
+            def patch(self):
+                pass
+
+            def delete(self):
+
+                post = Post.get_by_id(int(post_id))
+
+                post.state = 'deleted'
+                post.put()
+
+class UserNotifications(webapp2.RequestHandler):
+
+    def get(self, user_id):
+
+        user = User.get_by_id(int(user_id))
+
+        notifications = user.notifications
+
+        self.response.write(notifications)
+
+
+
 app = webapp2.WSGIApplication([
     ("/api/institution", SEU_HANDLER),
     ("/api/institution/:id", SEU_HANDLER),
@@ -28,7 +57,7 @@ app = webapp2.WSGIApplication([
     ("/api/institution/:id/followers", SEU_HANDLER),
     ("/api/institution/:id/timeline", SEU_HANDLER),
     ("/api/institution/:id/post", SEU_HANDLER),
-    ("/api/institution/:id/post/:id", SEU_HANDLER),
+    ("/api/institution/(\d+)/post/(\d+)", InstitutionPostHandler),
     ("/api/institution/:id/post/:id/comments", SEU_HANDLER),
     ("/api/user", SEU_HANDLER),
     ("/api/user/:id", SEU_HANDLER),
