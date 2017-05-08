7c47c77b84e0758dd1b6f9b6bdf4a5417accceb6
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2017-05-08 09:58:24 -0300

    Starting by user's authentication

--- raoni/ToDoList/main.py
+++ raoni/ToDoList/main.py
@@ -3,24 +3,34 @@ from model import *
 import json
 import datetime
 from utils import *
-
-
+from google.appengine.api import users
 
 
 class Handler(webapp2.RequestHandler):
 
     def get(self):
-        query = Task.query()
-        data = [todo.to_dict() for todo in query]
-        self.response.headers['Content-Type'] = 'application/json; charset=utf-8'
-        self.response.write(data2json(data))
+        user = users.get_current_user()
+        if user:
+            query = Task.query(Task.id_user == user.user_id())
+            data = [todo.to_dict() for todo in query]
+            self.response.headers['Content-Type'] = 'application/json; charset=utf-8'
+            self.response.write(data2json(data))
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
 
     def post(self):
-        data = json.loads(self.request.body)
-        task = Task()
-        task.name = data['name']
-        task.put()
-        self.response.set_status(201)
+        user = users.get_current_user()
+        if user:
+            data = json.loads(self.request.body)
+            task = Task()
+            task.name = data['name']
+            task.id_user = user.user_id()
+            task.put()
+            self.response.set_status(201)
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
 
 
 app = webapp2.WSGIApplication([
--- raoni/ToDoList/model.py
+++ raoni/ToDoList/model.py
@@ -2,6 +2,7 @@ from google.appengine.ext import ndb
 
 
 class Task(ndb.Model):
-    name = ndb.StringProperty(required=True);
-    date = ndb.DateProperty();
+    name = ndb.StringProperty(required=True)
+    date = ndb.DateProperty()
+    id_user = ndb.StringProperty(required=True)
 
