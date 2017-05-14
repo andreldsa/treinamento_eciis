d8a576a50d82c16b9b42951899862faf071df16d
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2017-05-14 20:54:07 -0300

    Refactoting taks view

---
+++ luiz/to-do-list/app/js/diretivas/task-directive.js
@@ -0,0 +1,12 @@
+(function() {
+    var app = angular.module("tarefasApp");
+    app.directive("uiTasksView", function() {
+        return {
+            templateUrl: "tasks-view.html",
+            restrict: "E",
+            scope: {
+                tasks: "="
+            }
+        }
+    });
+})();
